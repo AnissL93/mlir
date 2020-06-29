7f0f41e5873f7983ee2acba2b3b55d43b430a25f
lanhuiying <lanhuiying@cambricon.com>
2020-06-30 03:24:47 +0800

    modify td, add graph

--- mlir/examples/tfu/src/main.cc
+++ mlir/examples/tfu/src/main.cc
@@ -13,8 +13,10 @@
 #include "mlir/ExecutionEngine/OptUtils.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Module.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/IR/Attributes.h"
+#include "mlir/IR/OpDefinition.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Parser.h"
 #include "mlir/Pass/Pass.h"
@@ -30,6 +32,7 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/ScopedHashTable.h"
 
 namespace mlir {
 namespace tfu {
@@ -42,15 +45,22 @@ struct Module {
     F16 = builder.getF16Type();
   }
 
-  void MakeEntry(llvm::ArrayRef<Type> arg_types) {
+  void MakeEntry() {
     module = mlir::ModuleOp::create(builder.getUnknownLoc());
 
-    auto func_type = builder.getFunctionType(arg_types, llvm::None);
+    auto func_type = builder.getFunctionType({}, llvm::None);
     FuncOp entry_func = FuncOp::create(builder.getUnknownLoc(),
-                                       "test-conv-conv", func_type);
+                                       "main", func_type);
     auto entry_block = entry_func.addEntryBlock();
+
     module.push_back(entry_func);
     builder.setInsertionPointToStart(entry_block);
+  }
+
+  Block* MakeBlock(TypeRange arguments) {
+    Block *b = new Block();
+    builder.setInsertionPointToStart(b);
+    return b;
   }
 
   Value ConstTensor(TfuType t, const std::string &name) {
@@ -66,13 +76,9 @@ struct Module {
         builder.getStringAttr(name));
   }
 
-  FuncOp MakeFunc(llvm::StringRef name, Type ret_type,
-                 llvm::ArrayRef<Type> args) {
-    auto func_type = builder.getFunctionType(args, {ret_type});
-    FuncOp func = FuncOp::create(builder.getUnknownLoc(), name, func_type);
-    auto entry_block = func.addEntryBlock();
-    builder.setInsertionPointToStart(entry_block);
-    return func;
+  void Graph(llvm::ArrayRef<mlir::Type> ret_type, ValueRange inputs, Block* block) {
+    GraphOp op = builder.create<GraphOp>(builder.getUnknownLoc(), ret_type, inputs);
+    op.getRegion().push_back(block);
   }
 
   void Return(Value in) {
@@ -83,10 +89,13 @@ struct Module {
     module.dump();
   }
 
+
   Type F64, I64, F16;
   ModuleOp module;
   OpBuilder builder;
   MLIRContext* context;
+  llvm::ScopedHashTable<StringRef, mlir::Value> symbolTable;
+
 };
 
 }
@@ -99,23 +108,34 @@ void buildFuncConvConv() {
   OpBuilder builder(&context);
   Module module(&context, builder);
 
+  Block* b = new Block();
+  builder.setInsertionPointToStart(b);
+
   auto in_type = mlir::tfu::TfuType::get({1, 5, 5, 10}, "ddr", builder.getF16Type());
   auto w_type = mlir::tfu::TfuType::get({10, 1, 1, 10}, "ddr", builder.getIntegerType(8));
   auto out_type = mlir::tfu::TfuType::get({1, 5, 5, 10}, "ddr", builder.getF16Type());
+
   llvm::SmallVector<mlir::Type, 3> arg_types = {in_type, w_type, out_type};
-  module.MakeEntry(arg_types);
-  Value input = module.ConstTensor(in_type, "in");
-  Value w1 = module.ConstTensor(w_type, "w1");
-  Value w2 = module.ConstTensor(w_type, "w2");
 
-  module.MakeFunc("subgraph1", out_type,
-                  {input.getType(), w1.getType()});
-  Value conv1_out = module.Conv(input, w1, 1, "conv1");
-  module.Return(conv1_out);
-  Value conv2_out = module.Conv(conv1_out, w2, 1, "conv2");
-  module.Return(conv2_out);
+  // mek const tensor
+  auto init_attr = builder.getF64FloatAttr(0);
+  auto op1 = builder.create<mlir::tfu::ConstantOp>(builder.getUnknownLoc(), in_type, init_attr, "op1");
+  auto op2 = builder.create<mlir::tfu::ConstantOp>(builder.getUnknownLoc(), w_type, init_attr, "op2");
 
-  module.dump();
+  b->dump();
+
+//  module.MakeEntry();
+//
+//
+//  Value input = module.ConstTensor(in_type, "in");
+//  Value w1 = module.ConstTensor(w_type, "w1");
+//  Value w2 = module.ConstTensor(w_type, "w2");
+//  Value conv1_out = module.Conv(input, w1, 1, "conv1");
+//  module.Return(conv1_out);
+//  Value conv2_out = module.Conv(conv1_out, w2, 1, "conv2");
+//  module.Return(conv2_out);
+
+//  module.dump();
 }
 
 void buildConvConv() {
@@ -128,18 +148,15 @@ void buildConvConv() {
   auto in_type = mlir::tfu::TfuType::get({1, 5, 5, 10}, "ddr", builder.getF16Type());
   auto w_type = mlir::tfu::TfuType::get({10, 1, 1, 10}, "ddr", builder.getIntegerType(8));
   auto out_type = mlir::tfu::TfuType::get({1, 5, 5, 10}, "ddr", builder.getF16Type());
-  llvm::SmallVector<mlir::Type, 3> arg_types = {in_type, w_type, out_type};
-  module.MakeEntry(arg_types);
+  module.MakeEntry();
   Value input = module.ConstTensor(in_type, "in");
   Value w1 = module.ConstTensor(w_type, "w1");
   Value w2 = module.ConstTensor(w_type, "w2");
   Value conv1_out = module.Conv(input, w1, 1, "conv1");
   Value conv2_out = module.Conv(conv1_out, w2, 1, "conv2");
   module.Return(conv2_out);
-
   module.dump();
 }
-
 
 int main(int argn, char** argv) {
   mlir::registerAllDialects();
